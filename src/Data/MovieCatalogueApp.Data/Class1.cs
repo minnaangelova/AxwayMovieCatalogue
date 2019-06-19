15bd93285ed152d2b625ccabb2e6216cd421ea38
IvayloGugalov <ivaylogugalov@gmail.com>
2019-06-19 18:04:25 +0300

    new

--- src/Data/MovieCatalogueApp.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MovieCatalogueApp.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Data/MovieCatalogueApp.Data/MovieCatalogueAppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MovieCatalogueApp.Data
+{
+    public class Context: DbContext
+    {
+        public Context() : base("name = MovieCatalogueDBConnectionString")
+        {
+
+        }
+    }
+}
