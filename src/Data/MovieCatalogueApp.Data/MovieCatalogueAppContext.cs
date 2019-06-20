fb12801685e4300066aa4535426928dc7f6122b8
ItsoDimitrov <fffsssccc@abv.bg>
2019-06-20 15:48:35 +0300

    First migration created.

---
+++ src/Data/MovieCatalogueApp.Data/MovieCatalogueAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MovieCatalogueApp.Models.Entities;
+
+namespace MovieCatalogueApp.Data
+{
+    public class MovieCatalogueAppContext: DbContext
+    {
+
+        public MovieCatalogueAppContext(DbContextOptions<MovieCatalogueAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Actor> Actors{ get; set; }
+        public DbSet<Genre> Genres{ get; set; }
+        public DbSet<Movie> Movies{ get; set; }
+
+    }
+}
