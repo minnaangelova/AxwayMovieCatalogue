006a92d784f9b05d1adca3553386f63fd7798f39
IvayloGugalov <ivaylogugalov@gmail.com>
2019-06-26 11:51:14 +0300

    Add implementation for Search Movie with HTTP request

--- src/WPF/MovieCatalogueAppWPF/ViewModels/StartupViewModel.cs
+++ src/WPF/MovieCatalogueAppWPF/ViewModels/StartupViewModel.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using MovieCatalogueApp.Models.Entities;
 
 namespace MovieCatalogueAppWPF.ViewModels
 {
-    public class StartupViewModel
+    public class StartupViewModel: ViewModel
     {
-        
+        public ICommand SearchMovieCommand { get; set; }
+
+        public StartupViewModel()
+        {
+            SearchMovieCommand = new LambdaCommand(() =>
+            {
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri("http://localhost:12789/")
+                };
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var url = "api/movie";  //+ id;
+
+                HttpResponseMessage response = client.GetAsync(url).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var movie = response.Content.ReadAsAsync<Movie>().Result;
+                    MessageBox.Show(
+                        $"Movie Found {movie.Title} \n {movie.Genre} \n {movie.Rating} \n {movie.ReleaseDate} \n {movie.Summary} \n {movie.Poster}");
+                }
+                else
+                {
+                    MessageBox.Show($"Error code {response.StatusCode}");
+                }
+
+            });
+        }
     }
 }
--- src/WPF/MovieCatalogueAppWPF/Views/StartupView.xaml.cs
+++ src/WPF/MovieCatalogueAppWPF/Views/StartupView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MovieCatalogueAppWPF.ViewModels;
 
 namespace MovieCatalogueAppWPF
 {
@@ -20,6 +21,8 @@ namespace MovieCatalogueAppWPF
         public StartupView() 
         {
              InitializeComponent();
+
+             this.DataContext = new StartupViewModel();
         }
     }
 }
